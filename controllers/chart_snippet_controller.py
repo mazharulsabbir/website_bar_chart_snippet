896e4faf7b2cc1f8dbdd7a0dc8bd50648f79b80a
mazharulsabbir <mazharulsabbir2@gmail.com>
2022-08-20 15:04:37 +0600

    feat: read_group search updated and label is returned with dataset using api

--- controllers/chart_snippet_controller.py
+++ controllers/chart_snippet_controller.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import json
+from datetime import datetime, timedelta
 
 
 class WebsiteGraphSnippetController(http.Controller):
@@ -14,24 +16,66 @@ class WebsiteGraphSnippetController(http.Controller):
 
     @http.route('/api/website/snippet/model-info', auth='public', type='json', website=True)
     def model_info(self, **kw):
+        model_id = kw.get("model_id")
+        date_range = kw.get("dateRange")
+        day_frequency = kw.get("dayFrequency")
+
+        if not model_id or not date_range or not day_frequency:
+            return False
+
+        data_diff = datetime.now()-timedelta(days=int(date_range))
+        least_date = data_diff.strftime(r'%Y-%m-%d 23:59:59')
+
         graph_config = request.env['wbcs.graph_config'].sudo().search(
-            [('id', '=', int(kw.get('model_id')))], limit=1)
+            [('id', '=', int(model_id))],
+            limit=1
+        )
 
-        print("Model Id: ",graph_config.model_id)
+        if not graph_config:
+            return False
+
+        print("Model Id: ", graph_config.model_id.model)
+
+        domain = [
+            '&',
+            ('create_date', '>=', least_date),
+            ('create_date', '<=', datetime.now())
+        ]
+
+        count_records = []
+        count_label = []
         for field in graph_config.count_field_ids:
-            print(field)
+            groups = request.env[graph_config.model_id.model].read_group(
+                domain, [field.name+":count"], ['create_date:'+day_frequency]
+            )
+            print(groups)
+            # groups_count_mapping = dict((str(g[field.name+'count']), str(g['create_date'])) for g in groups)
+            data = {d['create_date_count'] for d in groups}
+            labels = {d['create_date:'+day_frequency] for d in groups}
+            count_records.extend(data)
+            count_label.extend(labels)
 
+        sum_records = []
+        sum_label = []
         for field in graph_config.sum_field_ids:
-            print(field)
+            groups = request.env[graph_config.model_id.model].read_group(
+                domain, [field.name+':sum'], ['create_date:'+day_frequency]
+            )
+            print(groups)
+            # data = [{
+            #     field.name: d['create_date_count'],
+            #     "create_date": d['create_date']
+            # } for d in groups]
+            data = {d['create_date_count'] for d in groups}
+            labels = {d['create_date:'+day_frequency] for d in groups}
 
-        # todo: return data here to load on bar chart
-        data = {
-            "name":"Mazharul Sabbir"
-        }
+            sum_records.extend(data)
+            sum_label.extend(labels)
 
         temp = request.env['ir.ui.view']._render_template(
-            'website_bar_chart_snippet.website_bar_chart_view',
-            data
+            'website_bar_chart_snippet.website_bar_chart_view'
         )
-        return {'template': temp}
-
+        labels = count_label+sum_label
+        labels = sorted(labels)
+        print(labels)
+        return {'template': temp, 'dataset': [count_records, sum_records], 'labels': labels}
--- static/src/js/bar_chart.js
+++ static/src/js/bar_chart.js
@@ -17,53 +17,6 @@ odoo.define('website_display_modules.website_display_modules_frontend', function
                 // console.log(attrs);
                 if (attrs != undefined) {
                     var realData = JSON.parse(attrs);
-                    const labels = [
-                        'January',
-                        'February',
-                        'March',
-                        'April',
-                        'May',
-                        'June',
-                    ];
-                    const dataset1 = {
-                        label: 'My First Dataset',
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 205, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(201, 203, 207, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgb(255, 99, 132)',
-                            'rgb(255, 159, 64)',
-                            'rgb(255, 205, 86)',
-                            'rgb(75, 192, 192)',
-                            'rgb(54, 162, 235)',
-                            'rgb(153, 102, 255)',
-                            'rgb(201, 203, 207)'
-                        ],
-                        borderWidth: 1
-                    };
-
-                    const data = {
-                        labels: labels,
-                        datasets: [dataset1, dataset1, dataset1]
-                    };
-                    const config = {
-                        type: 'bar',
-                        data: data,
-                        options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true
-                                }
-                            }
-                        },
-                    };
 
                     this._rpc({
                         route: '/api/website/snippet/model-info',
@@ -72,6 +25,52 @@ odoo.define('website_display_modules.website_display_modules_frontend', function
                         console.log(data);
                         self.$target.empty().append(`<canvas id="websiteBarChartSnippetCanvasId" height="100px"></canvas>`)
                         const ctx = document.getElementById('websiteBarChartSnippetCanvasId').getContext('2d');
+
+                        var _dataset = [];
+                        for (let index = 0; index < data.dataset.length; index++) {
+                            const element = data.dataset[index];
+
+                            const dataset1 = {
+                                label: index==0?'Count Dataset':'Sum Dataset',
+                                data: element,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)',
+                                    'rgba(255, 205, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(201, 203, 207, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgb(255, 99, 132)',
+                                    'rgb(255, 159, 64)',
+                                    'rgb(255, 205, 86)',
+                                    'rgb(75, 192, 192)',
+                                    'rgb(54, 162, 235)',
+                                    'rgb(153, 102, 255)',
+                                    'rgb(201, 203, 207)'
+                                ],
+                                borderWidth: 1
+                            };
+                            _dataset.push(dataset1)
+                        }
+                        
+                        const dataSet = {
+                            labels: data.labels,
+                            datasets: _dataset
+                        };
+                        const config = {
+                            type: 'bar',
+                            data: dataSet,
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            },
+                        };
                         const myChart = new Chart(ctx, config);
                     })
                 }
